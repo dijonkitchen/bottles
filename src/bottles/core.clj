c3a146d1dd5cb247dd21935897d1bb7216076c1e
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2019-06-16 21:48:33 -0700

    refactor: extract out verses function for ranges
    To better conform to original tests

--- src/bottles/core.clj
+++ src/bottles/core.clj
@@ -5,23 +5,29 @@
    [clojure.string :as string]))
 
 (defn verse
-  "Returns the verse `number` of the song.
+  "Returns the verse `number` of the song."
+  [number]
+  (case number
+    2 "2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall."
+
+    1 "1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall."
+
+    0 "No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall."
+
+    (format "%s bottles of beer on the wall, %s bottles of beer.
+Take one down and pass it around, %s bottles of beer on the wall."
+            number
+            number
+            (dec number))))
+
+(defn verses
+  "Returns a range of verses from start to finish, inclusive.
   Defaults to whole song."
   ([]
-   (verse 99 0))
-  ([number]
-   (case number
-     2  "2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall."
-     1  "1 bottle of beer on the wall, 1 bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall."
-     0 "No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall."
-     (format "%s bottles of beer on the wall, %s bottles of beer.
-Take one down and pass it around, %s bottles of beer on the wall."
-             number
-             number
-             (dec number))))
+   (verses 99 0))
   ([start finish]
    (->> start
         inc
@@ -33,7 +39,7 @@ Take one down and pass it around, %s bottles of beer on the wall."
 (defn song
   "Returns the whole song."
   []
-  (verse))
+  (verses))
 
 (defn -main
   "I don't do a whole lot ... yet."
