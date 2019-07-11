1b904da38ac723eae3d5579409da72b9485cbf44
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2019-07-11 08:24:47 -0700

    build: add cljfmt check

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :plugins [[jonase/eastwood "0.3.5"]
+            [lein-cljfmt "0.6.4"]
             [lein-cloverage "1.1.1"]]
   :main ^:skip-aot bottles.core
   :target-path "target/%s"
